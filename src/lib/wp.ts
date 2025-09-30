243bc31b5ab01e41c3a55d79c12d5b488c8df2c0
Freddy Romo <freddyromochavez@gmail.com>
2025-09-30 14:21:00 +0300

    Added tags/[tag] page

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -42,6 +42,22 @@ export const getAllPostsCategory = async () => {
     return categories
 }
 
+export const getAllPostsTags = async () => {
+    const response = await fetch(`${apiUrl}/tags?per_page=100`)
+    if (!response.ok) throw new Error("Failed to fetch latest posts")
+
+    const results = await response.json()
+    if (!results.length) throw new Error("No posts found")
+
+    const tags = results.map((tag: Category) => {
+        const { id, slug, name } = tag
+
+        return { id, slug, name }
+    })
+
+    return tags
+}
+
 export const getPostInfo = async (slug: string) => {
     const response = await fetch(`${apiUrl}/posts?slug=${slug}&_embed`)
     if (!response.ok) throw new Error("Failed to fetch page info")
@@ -91,6 +107,32 @@ export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}
 
 export const getPostsByCategory = async ({ perPage = 10 }: { perPage?: number } = {}, { categoryIds }: {categoryIds: number[]}) => {
     const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&categories=${categoryIds.join(",")}&_embed`)
+
+    if (!response.ok) throw new Error("Fail to fetch")
+
+    const results = await response.json()
+
+    if (!results.length) throw new Error("No Posts found")
+
+    const posts = results.map((post: WPPost) => {
+        const {
+            title: { rendered: title },
+            excerpt: { rendered: excerpt },
+            content: { rendered: content },
+            date,
+            slug
+        } = post
+
+        const featuredImage = post._embedded?.['wp:featuredmedia']?.[0]?.source_url ?? ""
+
+        return { title, excerpt, content, date, slug, featuredImage }
+    })
+
+    return posts
+}
+
+export const getPostsByTags = async ({ perPage = 10 }: { perPage?: number } = {}, { tagIds }: {tagIds: number[]}) => {
+    const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&tags=${tagIds.join(",")}&_embed`)
 
     if (!response.ok) throw new Error("Fail to fetch")
 
