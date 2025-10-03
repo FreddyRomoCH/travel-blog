629b49d7b2e57edee2581e1b55130f8d08dcb8f7
Freddy Romo <freddyromochavez@gmail.com>
2025-10-04 00:26:13 +0300

    removing throw error

--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -5,7 +5,10 @@ const apiUrl = isProd ? domain : `${domain}/wp-json/wp/v2`
 
 export const getPageInfo = async (slug: string) => {
     const response = await fetch(`${apiUrl}/pages?slug=${slug}&_embed`)
-    if (!response.ok) throw new Error("Failed to fetch page info")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const [data] = await response.json()
     const { title: { rendered: title }, content: { rendered: content } } = data
@@ -17,10 +20,16 @@ export const getPageInfo = async (slug: string) => {
 
 export const getAllPostsSlugs = async () => {
     const response = await fetch(`${apiUrl}/posts?per_page=100`)
-    if (!response.ok) throw new Error("Failed to fetch latest posts")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
-    if (!results.length) throw new Error("No posts found")
+    if (!results.length) {
+        console.error("No posts found")
+        return null
+    }
 
     const slugs = results.map((post: Post) => post.slug)
 
@@ -29,10 +38,16 @@ export const getAllPostsSlugs = async () => {
 
 export const getAllPostsCategory = async () => {
     const response = await fetch(`${apiUrl}/categories?per_page=100`)
-    if (!response.ok) throw new Error("Failed to fetch latest posts")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
-    if (!results.length) throw new Error("No posts found")
+    if (!results.length) {
+        console.error("No categories found")
+        return null
+    }
 
     const categories = results.map((category: Category) => {
         const { id, slug, name } = category
@@ -45,10 +60,16 @@ export const getAllPostsCategory = async () => {
 
 export const getAllPostsTags = async () => {
     const response = await fetch(`${apiUrl}/tags?per_page=100`)
-    if (!response.ok) throw new Error("Failed to fetch latest posts")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
-    if (!results.length) throw new Error("No posts found")
+    if (!results.length) {
+        console.error("No tags found")
+        return null
+    }
 
     const tags = results.map((tag: Category) => {
         const { id, slug, name } = tag
@@ -61,7 +82,10 @@ export const getAllPostsTags = async () => {
 
 export const getPostInfo = async (slug: string) => {
     const response = await fetch(`${apiUrl}/posts?slug=${slug}&_embed`)
-    if (!response.ok) throw new Error("Failed to fetch page info")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const [data] = await response.json()
     const { date, title: { rendered: title }, content: { rendered: content } } = data
@@ -84,10 +108,16 @@ export const getPostInfo = async (slug: string) => {
 
 export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}) => {
     const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&_embed`)
-    if (!response.ok) throw new Error("Failed to fetch latest posts")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
-    if (!results.length) throw new Error("No posts found")
+    if (!results.length) {
+        console.error("No posts found")
+        return null
+    }
 
     const posts = results.map((post: WPPost) => {
         const {
@@ -109,11 +139,17 @@ export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}
 export const getPostsByCategory = async ({ perPage = 10 }: { perPage?: number } = {}, { categoryIds }: {categoryIds: number[]}) => {
     const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&categories=${categoryIds.join(",")}&_embed`)
 
-    if (!response.ok) throw new Error("Fail to fetch")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
 
-    if (!results.length) throw new Error("No Posts found")
+    if (!results.length) {
+        console.error("No Posts found")
+        return null
+    }
 
     const posts = results.map((post: WPPost) => {
         const {
@@ -135,11 +171,17 @@ export const getPostsByCategory = async ({ perPage = 10 }: { perPage?: number } 
 export const getPostsByTags = async ({ perPage = 10 }: { perPage?: number } = {}, { tagIds }: {tagIds: number[]}) => {
     const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&tags=${tagIds.join(",")}&_embed`)
 
-    if (!response.ok) throw new Error("Fail to fetch")
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
 
-    if (!results.length) throw new Error("No Posts found")
+    if (!results.length) {
+        console.error("No Posts found")
+        return null
+    }
 
     const posts = results.map((post: WPPost) => {
         const {
@@ -160,10 +202,16 @@ export const getPostsByTags = async ({ perPage = 10 }: { perPage?: number } = {}
 
 export const getAllPosts = async ({ page = 1, perPage = 6 }: {page?:number, perPage?:number}) => {
     const response = await fetch(`${apiUrl}/posts?page=${page}&per_page=${perPage}&_embed`)
-    if (!response.ok) throw new Error('Error fetching data')
+    if (!response.ok) {
+        console.error(response)
+        return null
+    }
 
     const results = await response.json()
-    if (!results.length) throw new Error("No posts found")
+    if (!results.length) {
+        console.error("No posts found")
+        return null
+    }
 
     const totalPages = Number(response.headers.get("X-WP-TotalPages") || 1)
 
