d437354798dba2a6495ccd93457d0954b2821710
Freddy Romo <freddyromochavez@gmail.com>
2025-10-03 17:02:25 +0300

    Blog Site V1

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+export const menuLinks = [
+  { name: "Home", href: "/" },
+  { name: "Posts", href: "/my-posts/1" },
+  { name: "Categories", href: "/category" },
+  { name: "About Me", href: "/about" },
+];
