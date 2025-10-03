6e44adb4a572325dd5d57806b90e14da0b1ebfee
Freddy Romo <freddyromochavez@gmail.com>
2025-10-03 18:15:35 +0300

    Adding node mode standalone

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 export default defineConfig({
   // @ts-expect-error: viewTransitions not in types yet, but supported in runtime
@@ -16,4 +17,7 @@ export default defineConfig({
       },
     },
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
